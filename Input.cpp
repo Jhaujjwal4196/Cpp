3b8e94cdf9cbe4c5f00b7e78eed1cdbe035ed30e
Jhaujjwal4196 <ujjwaljh38@gmail.com>
2020-12-09 16:01:48 +0545

    FloatInputAddedForX

--- Input.cpp
+++ Input.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 main(){
     int age;
-    cout<< "Pleasde provide your age";
+    cout<< "Pleasde provide your age: \t";
     cin>> age;
-    cout<<"Your provided age in pr database is"<<age<<endl;
+    cout<<"Your provided age in pr database is:\t"<<age<<endl;
     
 }
