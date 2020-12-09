7a56b0f2845aa640d52e4249905a310aeb41ef13
Jhaujjwal4196 <ujjwaljh38@gmail.com>
2020-12-09 10:30:19 +0545

    inputadded

---
+++ Input.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+main(){
+    int age;
+    cout<< "Pleasde provide your age";
+    cin>> age;
+    cout<<"Your provided age in pr database is"<<age<<endl;
+    
+}
