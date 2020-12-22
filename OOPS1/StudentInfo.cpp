82ebb69a658e9d2ed26ec0647debaad586278281
Jhaujjwal4196 <ujjwaljh38@gmail.com>
2020-12-22 16:02:09 +0545

    CopyContructo

--- OOPS1/StudentInfo.cpp
+++ OOPS1/StudentInfo.cpp
@@ -2,12 +2,17 @@
 using namespace std;
 #include "Student.cpp"
 main(){
-    Student *s1= new Student;
+    Student *s1= new Student(20,4196,"Ujjwal",4187);
      
-     s1->setAge(-20);
-     s1->setRollNum(4196);
-     s1->setName("Ujjwal");
-    s1->display();
+    //  s1->setAge(-20);
+    //  s1->setRollNum(4196);
+    //  s1->setName("Ujjwal");
+    //s1->display();
+    cout<<s1->getAge()<<endl;
+    cout<<s1->getRegNo()<<endl;
+
+  Student *s2= new Student(*s1);
+  s2->display();
 
 
 }
