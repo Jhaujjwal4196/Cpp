68292e5b22799685eb6024e4560e45f6b3871fc6
Ujjwal  Jha <72433046+Jhaujjwal4196@users.noreply.github.com>
2020-12-22 14:22:28 +0545

    Update StudentInfo.cpp

--- OOPS1/StudentInfo.cpp
+++ OOPS1/StudentInfo.cpp
@@ -3,11 +3,8 @@ using namespace std;
 #include "Student.cpp"
 main(){
     Student *s1= new Student;
-     
-     s1->setAge(-20);
+      s1->setAge(-20);
      s1->setRollNum(4196);
      s1->setName("Ujjwal");
     s1->display();
-
-
 }
